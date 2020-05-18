0cdb681298e130a6626e43f56885f3356ddd80c4
Jackson Ramos <jackramos021495@gmail.com>
2020-05-18 14:37:14 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,11 @@
 class Triangle
   # write code here
+  def initialize(triangle_side1,triangle_side2,triangle_side3)
+    @triangle_side1 = triangle_side1
+    @triangle_side2 = triangle_side2
+    @triangle_side3 = triangle_side3
+  end
+
+  class TriangleError < StandardError
+  end
 end
